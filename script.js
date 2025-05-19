30f8a61dd4fb494c628e0462de6a6e4b22c7de22
Jermal Wynder <jermalwynder@gmail.com>
2025-05-19 11:28:57 -0400

    intialization/declaration of variables used to get reference from relevant DOM elements

---
+++ script.js
@@ -0,0 +1,11 @@
+// Selection of all necessary DOM elements
+const registrationForm = document.getElementById("registrationForm");
+const usernameInput = document.getElementById("username");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirmPassword");
+
+const usernameError = document.getElementById("usernameError");
+const emailError = document.getElementById("emailError");
+const passwordError = document.getElementById("passwordError");
+const confirmPasswordError = document.getElementById("confirmPasswordError");
